07a2917d8a8608ee981c2456c18dbfcf4424ed32
Kimmy <kimsungrak00@gmail.com>
2025-04-02 13:45:59 +0900

    oop

--- backend/src/main/java/CardRecommendService/memberCard/MemberCardListResponse.java
+++
@@ -1,7 +0,0 @@
-package CardRecommendService.memberCard;
-
-import CardRecommendService.card.Card;
-
-import java.util.List;
-
-public record MemberCardListResponse(List<MemberCardResponse> cards) {}
