9e79b7be82b613933a215bc8329eb565505a0152
Kimmy <kimsungrak00@gmail.com>
2025-04-14 11:17:36 +0900

    fix: CardBenefits 패키지 삭제

--- backend/src/main/java/CardRecommendService/cardBenefits/CardBenefitsRepository.java
+++
@@ -1,7 +0,0 @@
-package CardRecommendService.cardBenefits;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardBenefitsRepository extends JpaRepository<CardBenefits, Long> {
-
-}
